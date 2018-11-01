eedd8bd3f285f8d3fce36ccfb250b4eaf12487e5
skramer1323 <37364121+skramer1323@users.noreply.github.com>
2018-11-01 17:42:15 +0200

    Update docker-entrypoint.sh

--- 0.X/docker-entrypoint.sh
+++ 0.X/docker-entrypoint.sh
@@ -37,7 +37,7 @@ VAULT_CONFIG_DIR=/vault/config
 # You can also set the VAULT_LOCAL_CONFIG environment variable to pass some
 # Vault configuration JSON without having to bind any volumes.
 
-echo -e "backend "file" { path = "/vault/file/vaultsecrets" } listener "tcp" { address = "0.0.0.0:8200" tls_disable = 1 } " > /vault/config/local.json
+echo -e "listener "tcp" { address = "0.0.0.0:8200" tls_disable = 1 } " > /vault/config/local.json
 
 # If the user is trying to run Vault directly with some arguments, then
 # pass them to Vault.
